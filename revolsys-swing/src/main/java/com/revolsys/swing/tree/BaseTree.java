e2e42707b437e5239125d1854bc7ceab1d993168
Paul Austin <pauldaustin@users.noreply.github.com>
2023-06-19 12:36:28 -0700

    x

--- revolsys-swing/src/main/java/com/revolsys/swing/tree/BaseTree.java
+++ revolsys-swing/src/main/java/com/revolsys/swing/tree/BaseTree.java
@@ -20,7 +20,6 @@ import com.revolsys.collection.EmptyReference;
 import com.revolsys.collection.map.MapEx;
 import com.revolsys.properties.ObjectWithProperties;
 import com.revolsys.record.io.format.json.JsonObject;
-import com.revolsys.record.io.format.json.JsonObject;
 import com.revolsys.swing.menu.MenuFactory;
 import com.revolsys.swing.parallel.Invoke;
 import com.revolsys.swing.tree.dnd.TreeTransferHandler;
